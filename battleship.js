0ba5f75b0a2acc77a2a1c5737882b7a649d788ce
Sarah Foregger <sarahfo@Sarahs-MacBook-Pro-2.local>
2015-07-31 12:04:30 -0700

    added collision function

--- battleship.js
+++ battleship.js
@@ -36,11 +36,36 @@ var model = {
 
 	generateShip: function() {
 		//use math random to select whether its a horizontal or vertical ship
-		
+		var direction = Math.floor(Math.random() * 2);
+		var row; col;
+		if (direction ===1) {
+			row = Math.floor(Math.random() * this.boardSize);
+			col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+		}  else {
+			row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+			col = Math.floor(Math.random() * this.boardSize);
+		}
+		var newShipLocations = [];
+		for (var i = 0; i <this.shipLength; i++) {
+			if (direction === 1) {
+				newShipLocations.push(row + "" + (col + i));
+			} else {
+				newShipLocations.push((row + i) + "" + col);
+			}
+		}
+		return newShipLocations;
 	},
 
 	collision: function() {
-		// prevent collision of ships on placement
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = model.ships[i];
+			for (var j = 0, j < locations.length; j++) {
+				if (ship.locations.indexOf(locations[j]) >= 0) {
+					return true;
+				}
+			}
+		}
+		return false;
 	},
 
 	var ships = [{locations: [0, 0, 0], hits: ["", "", ""]},
